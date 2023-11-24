7f561f3ede12f66e218621f8478d07c5261cce7c
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-24 12:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
-
+let btn  = document.querySelector("button");
+let input = document.getElementById("guess");
+let para = document.getElementById("respond");
+let random = Math.floor(Math.random()*100) + 1;
+btn.addEventListener("click", onClick);
+function onClick(){
+	let number = parseInt(input.value);
+	if(number < random){
+		para.innerText = "guess higher";
+	}
+	else if(number > random){
+		para.innerText = "guess lower";
+	}
+	else{
+		para.innerText = "Correct guess";
+		btn.removeEventListener("click", onClick);
+		return;
+	}
+	input.value ="";
+}
